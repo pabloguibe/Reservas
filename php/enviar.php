c50abb2323236d2beee0cfe6e26d96859b604ce4
pablo guisasola <pabloguibe01@gmail.com>
2022-10-13 20:31:46 +0200

    htmlcompleto

---
+++ php/enviar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html">
+<head>
+    <meta charset="UTF-8">
+    <title>Base de datos</title>
+</head>
+<body>
+    <?php
+    //Conexion
+    $conexion = mysql_connect("localhost","root","");
+    //Control
+    if(!$conexion){
+    echo "No he podido conectar: ".mysql_error();
+    
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    ?>
+    
+</body>
+</html>
